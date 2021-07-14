a2cc88b7688a251f2c42a6a2e9ce4b6abdab331f
AbzG97 <Abdulla.ghazalah@hotmail.com>
2021-07-13 22:24:09 -0400

    fixed some code with .env and test.env

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const app = require("./index");
 
+
 const port = 3000 || process.env.PORT;
 
 app.listen(port, () => {
--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-
 const app = express();
 require("dotenv").config();
 const Article_Router = require("./routers/Article_Router");
